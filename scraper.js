29fad905490cd13e1f98908764ee45cd2c0e2f8c
DenilsonDesigns <daniellanguilller@gmail.com>
2018-07-09 20:24:06 +1000

    final exceeds

--- scraper.js
+++ scraper.js
@@ -11,6 +11,7 @@ const mainUrl= 'http://shirts4mike.com/shirts.php';
 const baseUrl= 'http://www.shirts4mike.com/';
 const pageUrls= [];
 const shirtData= [];
+
 //ASYNC/AWAIT METHOD
 async function getData(){
     try{
@@ -20,7 +21,7 @@ async function getData(){
         for(let i=0; i< shirts.data.shirts.length; i++){
             pageUrls.push(baseUrl+shirts.data.shirts[i].url);
         }
-        console.log(pageUrls);
+        //console.log(pageUrls);
 
         for(let i=0; i< pageUrls.length; i++){
             let pusher= await scrapeShirtPage(pageUrls[i]);
@@ -29,27 +30,29 @@ async function getData(){
             shirtData[i].title= shirtData[i].title.slice(9);
             shirtData[i].time= baseDate.toTimeString();
         }
-        console.log(shirtData);
+        //console.log(shirtData);
 
         //3. save data from pages to CSV file
         const csv= papa.unparse(shirtData);
-        console.log(csv);
+        //console.log(csv);
         if (!fs.existsSync('./data')){
                 fs.mkdirSync('./data');
             }   
         const writeCSV= fs.writeFileSync('data/'+csvFileName+'.csv', csv);
 
     }catch(err){
-        console.log('Error', err.message);
-        //write this error to a file scraper-error.log
-        fs.writeFileSync('./scraper-error.log', `${baseDate} Unable to retrieve data from https://shirts4mike.com 
-        Error code: ${err.message}`);
-    }
+        let errorMsg= `\n${baseDate} \nUnable to retrieve data from https://shirts4mike.com \nError code: ${err.message}`;
+        console.log(errorMsg, `Error code: ${err.message}`);
+        if(!fs.existsSync('./scraper-error.log')){
+            fs.appendFileSync('./scraper-error.log', errorMsg);
+        }else{
+            fs.appendFileSync('./scraper-error.log', '\n'+errorMsg);
+        }
+    } 
 }
 
 //MAIN CALL****
 getData();
-
 
 //HELPER FUNCTIONS******
 //this gets the url endings for each product page
@@ -87,17 +90,3 @@ function scrapeShirtPage(productUrl){
         reject(new Error('Failed scraping individual pages'));
     });
 }
-
-
-
-//*******MISC
-// console.log(shirts.data.shirts.length);
-// console.log(shirts.data.shirts[0].url);
-// console.log(baseUrl+shirts.data.shirts[0].url);
-
-// 2. function to scrape product pages and get info
-// const shirtData1= await scrapeShirtPage(pageUrls[0]);
-// shirtData1.data.url= pageUrls[0];
-// shirtData.push(shirtData1.data);
-// shirtData[0].title= shirtData[0].title.slice(9);
-// console.log(shirtData);
